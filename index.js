afd561094ad13a0a4de2d2d213797e88713e074a
arian2528 <arian2528@gmail.com>
2024-09-03 02:07:48 -0400

    Adding fastify options for adding corralationid header and refactoring create worker

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const fastify = require('fastify')({ logger: true, connectionTimeout: 5000 });
-const generateNewWorker = require('./utils/generateNewWorker');
+const fastify = require('fastify')({ logger: true, connectionTimeout: 5000, requestIdLogLabel: 'correlationId' });
+const getWorker = require('./utils/generateNewWorker');
 const requestTracker = require('./utils/requestTracker');
 
-const getCatsWorker = generateNewWorker('getCatsWorker');
-const getDogsWorker = generateNewWorker('getDogsWorker');
-
 fastify.get('/getCatsInfo', function handler (request, reply) {
+  const getCatsWorker = getWorker('getCatsWorker', Date.now());
   requestTracker[request.id] = (result) => reply.send(result)
   getCatsWorker.postMessage({ requestId: request.id});
 })
 
 fastify.get('/getDogsInfo', function handler (request, reply) {
+  const getDogsWorker = getWorker('getDogsWorker', Date.now());
   requestTracker[request.id] = (result) => reply.send(result)
   getDogsWorker.postMessage({ requestId: request.id });
 })
